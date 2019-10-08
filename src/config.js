f9a3c38f7355e4ec80e8d4afe8ac391e3d50bf8e
MinhChien98 <chiendev98@gmail.com>
2019-10-08 16:06:45 +0700

    update test

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
-export const BASE_API = 'http://localhost:4000/api';
-export const SOCKET_BASE = 'http://localhost:5000';
+export const BASE_API = "http://192.168.31.89:4000/api";
+export const SOCKET_BASE = "http://192.168.31.89:5000";
 
-export const REGISTER = 'users/signup';
-export const CHECK = 'players/check';
-export const LOGIN = 'auth';
+export const REGISTER = "users/signup";
+export const CHECK = "players/check";
+export const LOGIN = "auth";
 
-export const QUESTIONS = 'questionlists';
+export const QUESTIONS = "questionlists";
 
-export const PLAYGROUND = 'playgrounds';
-export const ANSWER = 'users/answer';
-export const PLAYED = 'playgrounds/played';
-export const PLAY = 'playgrounds/play';
+export const PLAYGROUND = "playgrounds";
+export const ANSWER = "users/answer";
+export const PLAYED = "playgrounds/played";
+export const PLAY = "playgrounds/play";
 
-export const ROUNDS = 'rounds';
-export const ROUNDS_AVAIABLE = 'rounds/available';
+export const ROUNDS = "rounds";
+export const ROUNDS_AVAIABLE = "rounds/available";
--- src/containers/AdminPage/AdminPage.js
+++ src/containers/AdminPage/AdminPage.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import './../../styles/play.css';
+import React, { Component } from "react";
+import "./../../styles/play.css";
 // import Modal from './../../components/modal';
-import * as types from './../../constants/ActionTypes';
-import { connect } from 'react-redux';
-import './../../styles/admin.css';
+import * as types from "./../../constants/ActionTypes";
+import { connect } from "react-redux";
+import "./../../styles/admin.css";
 
 // const socket = socketIOClient(SOCKET_BASE);
 class AdminPage extends Component {
-
   state = {
     questionList: [],
     currentQuestion: -1,
@@ -17,14 +16,14 @@ class AdminPage extends Component {
     next: true,
     ans: true,
     indexQues: 0,
-    canStartHelp: true,
-  }
+    canStartHelp: true
+  };
 
   componentWillMount() {
     const { history } = this.props;
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!token) {
-      history.push('/login');
+      history.push("/login");
     } else {
       this.props.onGetQuestionList();
       // this.props.onPlay();
@@ -34,8 +33,10 @@ class AdminPage extends Component {
   componentWillReceiveProps(props) {
     const { questionList, questions } = props;
     if (questionList && questionList.success) {
-      this.setState({ questionList: questionList.questionList })
-      this.props.onGetQuestionInList(questionList.questionList[this.state.currentList]._id);
+      this.setState({ questionList: questionList.questionList });
+      this.props.onGetQuestionInList(
+        questionList.questionList[this.state.currentList]._id
+      );
       this.props.onReserNoti();
     }
     if (questions && questions.success) {
@@ -46,16 +47,16 @@ class AdminPage extends Component {
 
   render() {
     const { currentQuestion, questions } = this.state;
-    const name = localStorage.getItem('name');
+    const name = localStorage.getItem("name");
     return (
       <div className="col-md-6 col-sm-11 container">
         {/* <div className="card cardCustom card-body"> */}
         <div className="col-md-12 col-sm-12 container card cardCustom">
+          <div className="col-md-5 col-sm-5">Xin chào {name}.</div>
           <div className="col-md-5 col-sm-5">
-            Xin chào {name}.
-                </div>
-          <div className="col-md-5 col-sm-5">
-            <button className="btn btn-danger" onClick={this.logOut}>Đăng xuất</button>
+            <button className="btn btn-danger" onClick={this.logOut}>
+              Đăng xuất
+            </button>
           </div>
         </div>
 
@@ -69,85 +70,153 @@ class AdminPage extends Component {
               <div className="card cardCustom">
                 Câu tiếp theo: {questions[currentQuestion + 1].questId.content}
               </div>
-            ) : ('')}
-
+            ) : (
+              ""
+            )}
           </React.Fragment>
-        ) : ('')}
+        ) : (
+          ""
+        )}
 
-        <button disabled={this.state.start} className="btn btn-primary" style={{ width: '100%' }} onClick={this.startGame}>Bắt đầu</button>
-        <button disabled={this.state.next} className="btn btn-primary" style={{ width: '100%' }} onClick={this.nextQuestion}>Câu kế tiếp</button>
-        <button disabled={this.state.ans} className="btn btn-primary" style={{ width: '100%' }} onClick={this.raiseAns}>Trả kết quả</button>
-        <button className="btn btn-primary" style={{ width: '100%' }}   onClick={this.changeQuestionList}>Thay bộ câu hỏi</button>
-        <button disabled={this.state.canStartHelp} className="btn btn-primary" style={{ width: '100%' }}  onClick={this.startHelp}>Bắt đầu cứu trợ</button>
+        <button
+          disabled={this.state.start}
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={this.startGame}
+        >
+          Bắt đầu
+        </button>
+        <button
+          disabled={this.state.next}
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={this.nextQuestion}
+        >
+          Câu kế tiếp
+        </button>
+        <button
+          disabled={this.state.ans}
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={this.raiseAns}
+        >
+          Trả kết quả
+        </button>
+        <button
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={this.changeQuestionList}
+        >
+          Thay bộ câu hỏi
+        </button>
+        <button
+          disabled={this.state.canStartHelp}
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={this.startHelp}
+        >
+          Bắt đầu cứu trợ
+        </button>
         {/* <button disabled={this.state.canStartHelp} className="btn btn-primary" style={{ width: '100%' }}  onClick={this.stopTime}>Dừng thời gian cứu trợ</button> */}
-        <button disabled={this.state.canStartHelp} className="btn btn-primary" style={{ width: '100%' }}  onClick={this.continueTime}>Tiếp tục thời gian</button>
-        <button disabled={this.state.canStartHelp} className="btn btn-primary" style={{ width: '100%' }}  onClick={this.showHelp}>Hiển thị kết quả cứu trợ</button>
+        <button
+          disabled={this.state.canStartHelp}
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={this.continueTime}
+        >
+          Tiếp tục thời gian
+        </button>
+        <button
+          disabled={this.state.canStartHelp}
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={this.showHelp}
+        >
+          Hiển thị kết quả cứu trợ
+        </button>
       </div>
     );
   }
 
   startGame = () => {
-    window.socketIO.emit('admin', { message: this.state.questions[0].questId._id, command: 1000 });
+    window.socketIO.emit("admin", {
+      message: this.state.questions[0].questId._id,
+      command: 1002
+    });
     this.setState({ currentQuestion: 0, start: true, ans: false });
-  }
+  };
 
   raiseAns = () => {
-    window.socketIO.emit('admin', { command: 4000 });
+    window.socketIO.emit("admin", { command: 4000 });
     this.setState({ next: false, ans: true });
-  }
+  };
 
   changeQuestionList = () => {
-    window.socketIO.emit('admin', { command: 4500 });
+    window.socketIO.emit("admin", { command: 4500 });
     const { currentList, questionList } = this.state;
     this.props.onGetQuestionInList(questionList[currentList + 1]._id);
-    this.setState({ currentList: currentList + 1, indexQues: -1, canStartHelp: false });
-  }
+    this.setState({
+      currentList: currentList + 1,
+      indexQues: -1,
+      canStartHelp: false
+    });
+  };
 
   showHelp = () => {
-    window.socketIO.emit('admin', { command: 3002 });
-  }
+    window.socketIO.emit("admin", { command: 3002 });
+  };
 
   startHelp = () => {
     const { indexQues } = this.state;
-    window.socketIO.emit('admin', { message: this.state.questions[indexQues].questId._id, command: 3001 });
-    window.socketIO.emit('admin', { command: 5000 });
-  }
+    window.socketIO.emit("admin", {
+      message: this.state.questions[indexQues].questId._id,
+      command: 3001
+    });
+    window.socketIO.emit("admin", { command: 5000 });
+  };
 
   continueTime = () => {
-    window.socketIO.emit('admin', { command: 5001 });
-  }
+    window.socketIO.emit("admin", { command: 5001 });
+  };
 
   nextQuestion = () => {
     const { indexQues } = this.state;
     this.setState({ indexQues: indexQues + 1, next: true, ans: false });
-    window.socketIO.emit('admin', { message: this.state.questions[indexQues + 1].questId._id, command: 1000 });
-  }
+    window.socketIO.emit("admin", {
+      message: this.state.questions[indexQues + 1].questId._id,
+      command: 1000
+    });
+  };
 
   logOut = () => {
-    localStorage.removeItem('studentId');
-    window.socketIO.emit('disconnect');
-    localStorage.removeItem('token');
-    localStorage.removeItem('name');
-    localStorage.removeItem('userId');
-    this.props.history.push('/login');
-  }
+    localStorage.removeItem("studentId");
+    window.socketIO.emit("disconnect");
+    localStorage.removeItem("token");
+    localStorage.removeItem("name");
+    localStorage.removeItem("userId");
+    this.props.history.push("/login");
+  };
 }
 
 const mapStateToProps = state => {
   return {
     questionList: state.questionList,
-    questions: state.questions,
-  }
-}
+    questions: state.questions
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     onGetQuestionList: () => dispatch({ type: types.GET_QUESTIONLIST }),
-    onGetQuestionInList: listId => dispatch({ type: types.GET_QUESTION_IN_LIST, listId }),
+    onGetQuestionInList: listId =>
+      dispatch({ type: types.GET_QUESTION_IN_LIST, listId }),
     onAnswer: answer => dispatch({ type: types.ANSWER, answer }),
-    onReserNoti: () => dispatch({ type: types.RESET_NOTI }),
+    onReserNoti: () => dispatch({ type: types.RESET_NOTI })
     // onPlay: () => dispatch({ type: types.PLAYED }),
   };
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(AdminPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AdminPage);
